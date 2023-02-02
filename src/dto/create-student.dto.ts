9e2bd687008b08400f8eca595f3f5803cb4771c3
WellRK <wellrokembach@gmail.com>
2023-02-01 23:10:26 -0300

    Config de DB, schema e DTO

---
+++ src/dto/create-student.dto.ts
@@ -0,0 +1,29 @@
+import { IsNotEmpty, IsNumber, IsString, MaxLength } from 'class-validator';
+
+export class CreateStudentDTO{
+    @IsString()
+    @MaxLength(30)
+    @IsNotEmpty()
+    readonly name:string;
+    
+    @IsNumber()
+    @IsNotEmpty()
+    readonly roleNumber: number;
+
+    @IsNumber()
+    @IsNotEmpty()
+    readonly class: number;
+
+    @IsString()
+    @MaxLength(30)
+    @IsNotEmpty()
+    readonly gender: string;
+
+    @IsNumber()
+    @IsNotEmpty()
+    readonly marks: number;
+
+
+
+}
+
