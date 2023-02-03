6c8f4a6c5c746798fc58db2c7d723be7c15d258d
WellRK <wellrokembach@gmail.com>
2023-02-03 12:08:35 -0300

    Metodo de criar usuario

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+  constructor(private readonly prisma: PrismaService){}
+  
+  async create(createUserDto: CreateUserDto) { 
+    const data = {
+        ...createUserDto,
+        password: await bcrypt.hash(createUserDto.password, 10),
+    };
 
-  findAll() {
-    return `This action returns all user`;
+   const createUser = await this.prisma.user.create({ data });
+    return data;
+    
   }
 
   findOne(id: number) {
     return `This action returns a #${id} user`;
   }
+  
+  
+  // findAll() {
+  //   return `This action returns all user`;
+  // }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+  // update(id: number, updateUserDto: UpdateUserDto) {
+  //   return `This action updates a #${id} user`;
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+  // remove(id: number) {
+  //   return `This action removes a #${id} user`;
+  // }
 }
