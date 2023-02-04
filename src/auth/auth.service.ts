d2bd8161ca75fcdcd2cc64d2bfb8f58e93e0c454
WellRK <wellrokembach@gmail.com>
2023-02-03 23:42:44 -0300

    Andamento do processo de verificacao de senha hash e db

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
-    validateUser(email: string, password: string) {
-        throw new Error('Method not implemented.');
+    constructor(private readonly userService: UserService){}
+
+
+    async validateUser(email: string, password: string) {
+        const user = await this.userService.findByEmail(email);
+
+        if(user){
+
+
+            
+        }
+
+
     }
 }
